29fe9422cc66d0940980a744c3d935635ca7dc56
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-04 19:40:59 +0530

    Added Beer Repository class.

---
+++ src/main/java/org/mourya/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package org.mourya.msscbeerservice.repositories;
+
+import org.mourya.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
