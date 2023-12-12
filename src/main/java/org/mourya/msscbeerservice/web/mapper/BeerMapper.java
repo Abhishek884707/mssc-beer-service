231de86bde45a6d9573cd9ddabd8d1a44ac3a3b7
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-13 00:18:02 +0530

    Resolved Failing Methods

--- src/main/java/org/mourya/msscbeerservice/web/mapper/BeerMapper.java
+++ src/main/java/org/mourya/msscbeerservice/web/mapper/BeerMapper.java
@@ -4,7 +4,7 @@ import org.mapstruct.Mapper;
 import org.mourya.msscbeerservice.domain.Beer;
 import org.mourya.msscbeerservice.web.model.BeerDto;
 
-@Mapper
+@Mapper(uses = DateMapper.class)
 public interface BeerMapper {
 
     BeerDto BeerToBeerDto(Beer beer);
