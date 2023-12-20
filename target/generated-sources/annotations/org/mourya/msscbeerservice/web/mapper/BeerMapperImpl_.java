885721f61ebb9d1c660a5fe7ab03bff53331d884
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-20 12:56:19 +0530

    pom file changes

--- target/generated-sources/annotations/org/mourya/msscbeerservice/web/mapper/BeerMapperImpl_.java
+++ target/generated-sources/annotations/org/mourya/msscbeerservice/web/mapper/BeerMapperImpl_.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-20T12:53:41+0530",
+    date = "2023-12-20T12:55:44+0530",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
