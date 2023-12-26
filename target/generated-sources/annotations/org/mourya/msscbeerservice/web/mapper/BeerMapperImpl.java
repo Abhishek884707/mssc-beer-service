c5d813d68c8b26347ba4faf32d15b10baf3063b9
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-26 17:01:20 +0530

    resolved integration test

--- target/generated-sources/annotations/org/mourya/msscbeerservice/web/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/org/mourya/msscbeerservice/web/mapper/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-22T11:56:17+0530",
+    date = "2023-12-26T17:00:21+0530",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
